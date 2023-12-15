3c3a7d63a1180889a0954a746ec60c0d74843547
Pandian004 <pandiaan1969@gmail.com>
2023-12-15 12:31:30 +0530

    added signup page and modified login page plus added asserts folder for signup page

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -1,13 +1,27 @@
+import'./signupPage.css';
+import {Link} from 'react-router-dom'
+
+const images = require.context('../../asserts',true);
+const imageList = images.keys().map(image => images(image));
+
+let imgsrc;
+function changeImg(){
+    imgsrc = imageList[Math.floor(Math.random()*imageList.length)];
+  }
 function SignupPage(){
     return(
-        <div className="signupForm">
-            <form>
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-            </form>
+        <div className="signupForm" onLoad={changeImg()}>
+            <img src={imgsrc} alt="pic"/>
+            <div className="form">
+                <h3>Hello, new traveller!</h3>
+                <form>
+                    <input type='text' placeholder='Enter your name' /><br /><br />
+                    <input type='email' placeholder='Enter your email' /><br /><br />
+                    <input type='password' placeholder='Create your password' /><br /><br />
+                    <input type='password' placeholder='Reapeat your password' /><br /><br />
+                    <Link to="/"><button type='submit'>Sign Up</button></Link>
+                </form>
+            </div>
         </div>
     );
 }
