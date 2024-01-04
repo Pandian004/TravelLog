ddf5b4532dac8b6effa5b1c3bcd966506d044efe
Pandian004 <pandiaan1969@gmail.com>
2024-01-04 17:03:42 +0530

    Some major change

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,8 +1,5 @@
 import {Link} from 'react-router-dom';
-import './loginPage.css'
-import { useState } from 'react';
-import Eye from '../AssertsNew/eye.png'
-import Crossedeye from '../AssertsNew/crossed-eye.png'
+import './loginPage.css';
 
 const images = require.context('../../LoginPhotos',true);
 const imageList = images.keys().map(image => images(image));
@@ -14,16 +11,19 @@ function changeImg(){
  const d = new Date();
 let year = d.getFullYear();
 
-function LoginPage(){   
-    const [visible, setVisible] = useState(false);  
+function LoginPage(){    
     return(
         <div className="images" onLoad={changeImg()} >
             <img src={imgsrc} alt="pic"  />
             <div className='loginForm'>
                 <h3>Welcome back, Traveller!</h3>
                 <form>
-                    <input type="email" placeholder="Enter email" required /><br /><br />
-                    <input type={visible?'text':'password'} placeholder="Enter password"  required /> <br /><br />
+                    <div>
+                        <input type="email" placeholder="Enter email" required /><br /><br />
+                    </div>
+                    <div>
+                        <input type='password' placeholder="Enter password" required /> <br /><br />
+                    </div>
                     <Link to="/"><button type="submit"><strong>Login</strong></button></Link>
                 </form>
                 <p>Copyright &#169; <Link id='copy' to="/">Travel Log </Link>  {year}</p>
