d821511688ff929e732c8eb73aa19b8b118b3610
Pandian004 <pandiaan1969@gmail.com>
2024-01-08 11:32:01 +0530

    Updated update profile page

--- client/src/pages/AddInfo.js
+++ client/src/pages/AddInfo.js
@@ -12,26 +12,36 @@ function AddInfo(){
     const onCrop = view => {
         setPreview(view);
     }
-    console.log(preview)
+    console.log(preview);
+    const [showModal, setShowModal] = useState(false);
+    const [exist, setExist] = useState(false);
     return(
         <div className="avatar-addInfo">
             <h3>Update your profile</h3>
             <hr/>
             <div className="Avatar">
-                <Avatar 
-                    height={400}
-                    width={400}
-                    imageWidth={400}
-                    label={"Select a profile photo"}
-                    onCrop={onCrop}
-                    onClose={onClose}
-                    src={null}
-                />
-                <div className="Info">
-                    <img src={preview} alt='/' className='ProfilePic'/>
+            <img src={preview} alt='/' className='ProfilePic' onClick={()=>{setShowModal(!showModal)}}/>
+                {showModal && 
+                    <div className="modal-Update" id="modal">
+                        <div className="modal-content-Update">
+                            <Avatar 
+                                height={400}
+                                width={600}
+                                imageHeight={400}
+                                label={"Select a profile photo"}
+                                onCrop={onCrop}
+                                onClose={onClose}
+                                src={null}
+                            /><br/>
+                            <button id='Save-btn' type='submit' onClick={()=>{setShowModal(!showModal)}}>Save</button>
+                        </div>
+                    </div>
+                }
+                <div className="Info" onClick={() => {setExist(exist)}}>
                     <form>
-                        <input type='text' placeholder='Pick a Username ' required/><br/><br/>
-                        <textarea placeholder='About yourself...(Optional)' rows={5} /><br/>
+                        {exist?<h4 id='Warning'>* Username already exist</h4>:" "}
+                        <input type='text' placeholder='Pick a Username ' required/>
+                        <textarea placeholder='About yourself...(Optional)' rows={5} />
                         <Link to="/"> <button type='submit'><strong>Update</strong></button></Link>
                     </form>
                 </div>
